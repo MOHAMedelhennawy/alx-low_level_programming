7c7b0c087777c156e61c98a6a97360405a6ffeb1
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-13 15:35:21 -0700

    initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* 
+ * comming
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int mian(void)
+{
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a int: %d byte(s)",sizeof(int));
+	printf("Size of a long: %d byte(s)",sizeof(long int));
+	printf("Size of a long long int: %d byte(s)",sizeof(long long int));
+	printf("Size of a float: %d byte(s)",sizeof(float));
+	return(0);
