24d57710172aa34e57f4ea2899b321eb6cc39174
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-13 15:41:30 -0700

    init commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-/*
-Hello
-*/
+
+/**
+ * main -  prints exactly "Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	retrun(0);
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-/* 
- * comming
- * */
+
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
