79851a5dfd057faed30e5d8d88a948f88fcc49b8
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 16:36:02 -0700

    initial commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
-		printf("Last digit of %d is and is greater than 5", n);
+		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
 	else if (n % 10 == 0)
-		printf("Last digit of %d is and is 0", n);
+		printf("Last digit of %d is %d and is 0", n, n % 10);
 	else
-		printf("Last digit of %d is and is less than 6 and not 0", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
 	return (0);
 }
