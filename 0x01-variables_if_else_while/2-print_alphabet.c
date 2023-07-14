6aae11e8ffc1127530198e68e9d8fdfdbabb1df2
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 16:46:32 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i <= 122 ; i++)
+		putchar(i);
+	printf("\n");
+}
