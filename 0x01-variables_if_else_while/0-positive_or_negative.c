ba4a91e853389592d0a2599983f4d4ea8d49f001
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 15:50:27 -0700

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,11 @@ int main()
 	srand(time(0));
 	int n= rand() - RAND_MAX / 2;
 	
-    	if(n==0)        printf("%d is zero\n",n);
-	else if (n>0)   printf("%d is positive\n",n);
-        else            printf("%d is negativ\n",n);
+    	if (n>0)   
+		printf("%d is positive\n",n);
+	else if(n==0)
+	     	printf("%d is zero\n",n);
+	else 
+     		printf("%d is negativ\n",n);
 	return (0);
 }
