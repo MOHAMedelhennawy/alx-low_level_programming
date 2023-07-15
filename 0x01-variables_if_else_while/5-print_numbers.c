fb42a1416b7e34728b58647bdbb319848768bc43
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 17:39:13 -0700

    initial commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the alphabet in lowercase,
- *   * followed by a new line
- *    * Return: Always 0 (Success)
- *     */
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i<10; i++)
+	for (i = 0; i < 10; i++)
 		printf("%d", i);
+	printf("\n");
 	return (0);
 }
