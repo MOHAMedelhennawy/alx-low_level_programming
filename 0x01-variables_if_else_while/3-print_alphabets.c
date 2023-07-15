63aad68fe2c84c3ad9246ac086a8c244d04bdc64
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 17:09:58 -0700

    initial commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,12 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-        {
-                putchar(i);
-        }
+	for (i = 97; i <= 122; i++)
+		putchar(i);
 	for (i = 65; i <= 90; i++)
-        {
                 putchar(i);
-        }
         putchar('\n');
         return (0);
 }
