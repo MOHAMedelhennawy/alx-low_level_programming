458b37b88d1a09e058c122e538c93dcc29fa76f6
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 18:28:59 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i < 90; i++)
+	{
+		if (i < 10)
+		{
+			printf("0%d", i);
+		}
+		else
+		{
+			if (i % 10 == (i % 10)*10+(i % 10) || i % 10 == 0)
+				continue;
+			else
+				printf("%d", i);
+		}
+		if (i < 89)
+			printf(", ");
+	}
+}
