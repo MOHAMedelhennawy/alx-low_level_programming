8e0d08e89bf249334fe25bbeb63cd82d24a9f698
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 17:44:09 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
