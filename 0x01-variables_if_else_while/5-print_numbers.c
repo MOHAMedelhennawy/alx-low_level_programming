520c58e489726efa114ad059faea28665c2889e1
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 17:35:03 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints the alphabet in lowercase,
+ *   * followed by a new line
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i<10; i++)
+		printf("%d", n);
+	return (0);
+}
