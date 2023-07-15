41972d2ea7dd24f10ed235706f3dc8e7c188a236
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-14 17:28:34 -0700

    initial commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        char ch;
-        
-        for(ch = 'a'; ch < 'z'; ch++)
-        {       
-                if(ch == 'q' || ch == 'e')
-                        continue;
-                else
-                        putchar(ch);
-        }
+{
+	char ch;
+
+	for(ch = 'a'; ch < 'z'; ch++)
+	{
+		if(ch == 'q' || ch == 'e')
+			continue;
+		else
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
