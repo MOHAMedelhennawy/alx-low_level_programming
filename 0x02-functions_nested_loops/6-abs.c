b227a5404ebf8f62fc8f2f880613e04bb476935f
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-18 03:48:11 -0700

    initial commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - calculate the absolute value of the number from zero.
- * @i: the integer to be converted.
+ * @n: the integer to be converted.
  * Return: absolute value of the input number.
  */
 
