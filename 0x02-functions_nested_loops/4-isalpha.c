22266bf30bd9fc7113e241b24b02cf30f20c05ac
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-18 02:43:28 -0700

    initial commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if (c => 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
-	if (c => 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (1);
