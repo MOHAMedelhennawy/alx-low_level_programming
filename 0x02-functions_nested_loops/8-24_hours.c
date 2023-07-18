e76555f0907c77dab2ddab78cd42a8ecf477c3aa
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-18 16:50:37 -0700

    initial commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,31 +5,18 @@
  */
 void jack_bauer(void)
 {
-	int H = 0;
-	int M = 0;
+	int H , M;
 
-	while (H < 24)
+	for (H = 0; H < 24; H++)
 	{
-		if (H < 10)
+		for (M = 0; M < 60; M++)
 		{
-			_putchar('0');
-			_putchar(H + '0');
+			_putchar(H / 10 + '0');
+			_putchar(H % 10 + '0');
+			_putchar(':');
+			_putchar(M / 10 + '0');
+			_putchar(M % 10 + '0');
+			_putchar('\n');
 		}
-		else
-			_putchar(H + '0');
-		_putchar(':');
-		while (M < 60)
-		{
-			if (M < 10)
-			{
-				_putchar('0');
-				_putchar(M + '0');
-			}
-			else
-				_putchar(M + '0');
-			M++;
-		}
-		H++;
-		_putchar('\n');
 	}
 }
