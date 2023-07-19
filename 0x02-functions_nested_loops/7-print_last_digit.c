62853193da5135fb45a9b107567cdd579f707d0e
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-18 18:06:02 -0700

    initial commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,12 @@
  */
 int print_last_digit(int n)
 {
+	int last;
+
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+	_putchar(last + '0');
+	return (last);
 }
