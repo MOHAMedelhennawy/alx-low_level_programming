3d24c3f8f9b838fa5f88455730d3e8f224d851a3
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-18 17:32:04 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
