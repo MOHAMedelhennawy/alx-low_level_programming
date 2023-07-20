7e31f79f63dcc83e1915644c0ce656b21f08d6ab
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-20 02:25:32 -0700

    test

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-    	if (a > b && a > c)
-    	{
+	if (a > b && a > C)
+	{
 		largest = a;
-    	}
+	}
 	else if (b > a && b > c)
-    	{
-        	largest = b;
-    	}
-    	else
-    	{
-        	largest = c;
-    	}
-
-    	return (largest);
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
