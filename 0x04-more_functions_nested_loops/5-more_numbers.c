5de30ada34eaed7e0734050f2475f4183f822a1f
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-20 03:25:37 -0700

    initial commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,20 @@
   */
 void more_numbers(void)
 {
-	int i, j;
+	int x, y;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x < 10; x++)
 	{
-		for (j = 0; j < 15; j++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else
+			if (y > 9)
 			{
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
+				_putchar((y / 10) + '0');
 			}
+
+			_putchar((y % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }
