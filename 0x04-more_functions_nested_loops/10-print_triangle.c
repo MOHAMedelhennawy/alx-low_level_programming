82efb4706927e6aea223f762b3d960fe9708be3b
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-20 03:36:50 -0700

    initial commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,9 +8,12 @@ void print_triangle(int size)
 {
 	int i, j;
 	int k = size - 1;
-	for(i = 0; i < size; i++)
-   	{
-		for(j = 0 ; j < size; j++)
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
 			if (k <= j)
 				_putchar('#');
