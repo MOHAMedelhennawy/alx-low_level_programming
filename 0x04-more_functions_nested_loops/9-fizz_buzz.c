23f02b05eb3c25d6b6f2110f44d30ae9973c4394
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-20 21:11:51 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fizz buzz
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ",i);
+	}
+	putchar('\n');
+	return (0);	
+}
---
+++ 0x04-more_functions_nested_loops/fizz_buzz_main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	print_FizzBuzz();
+	return (0);
+}
