6bdd3809e9beb1a2a39e4141a8b20961113b9e66
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-23 21:14:02 -0700

    tes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
