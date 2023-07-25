7b85b97f6298760003ba424ba9b3b825f22d2d7b
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-24 21:28:58 -0700

    initial commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,18 @@
 #include "main.h"
-
+#include <string.h>
+/**
+ * rev_string - prints in reverse a string.
+ * @s: tested string
+ * Return: always 0.
+ */
 void rev_string(char *s)
 {
 	int l, i;
 	char *end, *start, tmp;
 
 	l = strlen(s) - 1;
-	end = *(s + l);
-	start = *s;
+	end = s + l;
+	start = s;
 
 	for (i = 0; i < (l + 1) / 2; i++)
 	{
@@ -17,5 +22,4 @@ void rev_string(char *s)
 		start++;
 		end--;
 	}
-	
 }
