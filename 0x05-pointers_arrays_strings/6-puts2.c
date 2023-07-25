8179a49601226f8bd782792f4475c91cc9f5de84
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-24 22:40:35 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ *puts2 - prints every other char
+ *@str: char*
+ *Return: void
+ */
+void puts2(char *str)
+{
+	int l = strlen(str) - 1;
+	int i;
+	char *ch;
+
+	for (i = 0; i < l; i += 2)
+	{
+		ch = str[i];
+		_putchar(ch);
+	}
