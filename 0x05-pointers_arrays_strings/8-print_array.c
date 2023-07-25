db1e239930545996579e82e77ee0d70d35eb302c
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-25 02:04:32 -0700

    initial commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n - 1)
-		{
-			printf(", ");
-		}
-		else
-			printf("\n");
+		if (i == n - 1)
+			break;
+		printf(", ");
 	}
+	printf("\n");
 }
