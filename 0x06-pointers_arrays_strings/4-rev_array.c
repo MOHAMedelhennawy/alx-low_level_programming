d01f63c3a95c09d638ff9e8b5d265cb4d383899e
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-26 11:34:50 -0700

    initial commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,15 +10,10 @@ void reverse_array(int *a, int n)
 	int i, end = n - 1;
 	int tmp;
 
-	if (n % 2 == 0)
-		n = n / 2;
-	else
-		n = (n / 2) - 1;
-
-	for (i = 0; i <= n; i++, end--)
+	for (i = 0; i < n / 2; i++, end--)
 	{
 		tmp = a[i];
-		a[i] = a[end];
-		a[end] = tmp;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
 	}
 }
