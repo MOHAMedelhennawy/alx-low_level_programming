63aa2da35fdf027fe07c2944a086d54bdd3cdfe8
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-26 09:08:40 -0700

    initial commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = -1;
 
-	while (s1[++i] != '\0')
+	while (s1[++i] != '\0' && s2[++1] != '\0')
 	return (s1[i] != s2[i] ? s1[i] - s2[i] : s1[i] - s2[i]);
 
 	return (0);
