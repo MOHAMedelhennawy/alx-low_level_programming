0402d0feabc75ada66b903ad2d3d0a36014cfc00
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 05:33:13 -0700

    initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,4 +2,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	vi
+	int i = -1;
+
+	while (++i < n && src[i] != '\0')
+		dest[i] = src[i];
+	if (i < n)
+		dest[i] = '\0';
+
+	return (dest);
+}
