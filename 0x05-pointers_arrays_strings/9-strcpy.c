dc224df45bfc41697c1e8b53fd1410b606c2e847
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 09:23:35 -0700

    initial commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-    
-	dest[i + 1] = '\0';
-	return dest;
+	des [i] = '\0';
+	return (dest);
 }
