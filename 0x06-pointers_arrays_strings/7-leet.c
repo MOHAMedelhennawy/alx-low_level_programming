f01917d23deee4a0462955ad049f4893b06d0965
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 04:25:32 -0700

    initial commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,22 +10,22 @@ char *leet(char *s)
 {
 	int i;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) == 'a' || *(s + i) == 'A')
-			*(s + i) = '4';
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
 
-		else if (*(s + i) == 'e' || *(s + i) == 'E')
-			*(s + i) = '3';
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
 
-		else if (*(s + i) == 'o' || *(s + i) == 'O')
-			*(s + i) = '0';
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
 
-		else if (*(s + i) == 't' || *(s + i) == 'T')
-			*(s + i) = '7';
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
 
-		else if (*(s + i) == 'l' || *(s + i) == 'L')
-			*(s + i) = '1';
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
 	}
 
 	return (s);
