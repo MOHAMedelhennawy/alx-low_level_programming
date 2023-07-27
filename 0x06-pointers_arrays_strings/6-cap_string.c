32e0ea81f10aa7ecd4f1d7386f98eda35b98b6dd
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 06:35:13 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char *a = ",;.!?() {}\"\t\n";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			{
+			s[i + 1] -= 32;
+			}
+		}
+	}
+	return (s);
+}
