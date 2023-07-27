ba6d77e1b43205b08e868da753b0c95117ae23e4
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 04:36:13 -0700

    initial commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,20 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
 	}
 
 	return (s);
