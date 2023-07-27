865495beb09f8e6c7c503069edc975c924e65538
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 05:36:49 -0700

    initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ * _strncpy - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = -1;
 
 	while (++i < n && src[i] != '\0')
 		dest[i] = src[i];
-	
+
 	while (i < n)
 	{
 		dest[i] = '\0';
