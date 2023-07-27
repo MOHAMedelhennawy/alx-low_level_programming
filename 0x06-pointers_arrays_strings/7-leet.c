ecf0e2093aa1ddb6087d887deb46893096bd4a72
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-27 04:17:01 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == 'a' || *(s + i) == 'A')
+			*(S + i) = '4';
+
+		else if (*(s + i) == 'e' || *(s + i) == 'E')
+			*(s + i) = '3';
+
+		else if (*(s + i) == 'o' || *(s + i) == 'O')
+			*(s + i) = '0';
+
+		else if (*(s + i) == 't' || *(s + i) == 'T')
+			*(s + i) = '7';
+
+		else if (*(s + i) == 'l' || *(s + i) == 'L')
+			*(s + i) = '1';
+	}
+
+	return (s);
+}
