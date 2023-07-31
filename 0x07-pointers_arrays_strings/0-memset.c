427f5a17d9d424f6cfea4088301e63bdbd9c68c9
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-07-31 08:24:40 -0700

    initial commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i = -1;
 
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
+	while (++i < n)
+	*(s + i) = b;
 
 	return (s);
 }
