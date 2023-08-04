13e7a7ff03957deb2b9915a6f0372cf72bbecf12
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-03 18:43:00 -0700

    initial commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,8 +11,10 @@ int _sqrt_recursion(int n)
 {
 	if (n < 1)
 		return (-1);
+
 	else if (n == 1)
 		return (1);
+
 	else
 		return (sqroot(n, 2));
 
@@ -28,9 +30,9 @@ int sqroot(int n, int i)
 {
 	if (i * i == n)
 		return (i);
+
 	else if (i == n / 2)
 		return (-1);
 
 	sqroot(n, i + 1);
-
 }
