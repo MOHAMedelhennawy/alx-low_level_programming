9088fb65a7da3c2f99c963dc67ee74e47d81436e
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-06 19:57:57 -0700

    initial commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, multi = 1;
+	int i, j;
 
-	for (i = 1; i < argc; i++)
-		multi *= atoi(argv[i]);
-
-	if (argc > 1)
-		printf("%d\n", multi);
-	else 
+	if (argc == 1 || argc == 2)
+	{
 		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
 	return (0);
 }
