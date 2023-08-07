e821e27c2dd553043604b31470615b0c72f032f5
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-07 00:31:45 -0700

    initial commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef main.h
+#define main.h
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
+#endif
