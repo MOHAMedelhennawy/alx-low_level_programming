3eb40f1a2329e3394b736c2dbc090a50ff3a9f09
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-06 19:28:42 -0700

    initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *
  * Return: always 0.
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
