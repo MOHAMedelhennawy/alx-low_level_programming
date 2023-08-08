7db0b0a8e9d1fd907a7b1ff7708d5b27ed5513cb
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-07 20:30:02 -0700

    initial commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup- copy a string.
+ * @str: string to be copied.
+ *
+ *
+ * Return: pointer to the copied string.
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	ptr = (char *)malloc(sizeof(char));
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+	return (ptr);
+}
