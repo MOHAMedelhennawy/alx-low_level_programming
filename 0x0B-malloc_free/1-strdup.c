76fbfaa8fbb5d87facfdde226f28ac6c74ca9ea3
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-08 22:21:11 -0700

    initial commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,19 +9,15 @@
  */
 char *_strdup(char *str)
 {
-	int strSize, i;
-	char *copyStr;
+	char *ptr = (char *)malloc(sizeof(char));
+	int i;
 
-	strSize = 0;
 	if (str == NULL)
 		return (NULL);
-	while (*(str + strSize))
-		strSize++;
-	strSize++; /* add 1 to print NULL*/
-	copyStr = malloc((strSize) * sizeof(char));
-	if (copyStr == NULL)
-		return (0);
-	for (i = 0; i < (strSize); i++)
-		*(copyStr + i) = *(str + i);
-	return (copyStr);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		*(ptr + i) = str[i];
+	}
+	return (ptr);
 }
