54fbc382fbef5a9b60c57005353a029878fbd514
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-09 00:52:27 -0700

    initial commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = "";
+		*s1 = "";
 	if (s2 == NULL)
-		s2 = "";
+		*s2 = "";
 
 	ptr = malloc(sizeof(char) * sum);
 	if (ptr == NULL)
