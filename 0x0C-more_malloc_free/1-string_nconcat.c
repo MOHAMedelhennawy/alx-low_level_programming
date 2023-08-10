bb7de4922fca95fb566b1b9ddcc6f56b9728f205
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-10 02:51:13 -0700

    initial commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,23 +9,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int k = 0;
-	unsigned int i, len1 = 0, len2 = 0;
+	unsigned int i, len = 0;
 	char *ptr;
 
 	while (s1[len])
-		len1++;
-	while (s2[len2])
-		len2++;
+		len++;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n >= len2)
-		n = lne2;
-
-	ptr = malloc((len + n + 1) * sizeof(char));
+	ptr = malloc((len + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 
