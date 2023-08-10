64c0de022a1ba7d1ebb3d18fb40591289c90bb67
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-10 02:58:54 -0700

    initial commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len; i++, min++)
+	for (i = 0; i < len; i++, min++)
 		*(ptr + i) = min;
 
 	return (ptr);
