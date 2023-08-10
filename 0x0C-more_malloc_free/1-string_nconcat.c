d4de4a3106f608328ba7395be56ee06d4ed632b5
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-10 02:37:44 -0700

    initial commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,10 +27,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len; i++)
 		*(ptr + i) = *(s1 + i);
 
-	for (i = len; i < n + len && s2[k] != '\0'; i++, k++)
-	{
+	for (i = len; i < n + len; i++, k++)
 		*(ptr + i) = *(s2 + k);
-	}
 
 	*(ptr + i) = '\0';
 	return (ptr);
