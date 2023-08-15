cf461eb39fff99bda6af046899ccbd3de0017c28
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-15 10:03:33 -0700

    initial commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+/**
+ * print_dog - to print element of struct
+ * @d: the struct
+ * Return: info
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	if (!d->name)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %d\n", b->name);
+
+	if (!d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %d\n", b->age);
+
+	if (!d->owner)
+		printf("Owner: (nil)\n");
+	else
+		printf("Oge: %d\n", b->Owner);
+
