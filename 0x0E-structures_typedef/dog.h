9d9d6ac3db649910e4be7fa5c4da45eb141b834f
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-15 10:22:51 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,8 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /*_DOG_H_*/
