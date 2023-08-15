10d89e63571db1c6bf2f97de1d79918702ef8772
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-15 10:11:39 -0700

    initial commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,10 +14,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (!d->age)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (!d->owner)
 			printf("Owner: (nil)\n");
