6728693400e37d4a609ac92adc026c7f368925b4
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-15 09:47:21 -0700

    initial commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include "dog.h"
+/**
+ * init_dog - initilizes of struct
+ * @d: the struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-        d->name = name;
-        d->age = age;
-        d->owner = owner;
+	if (!d)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
