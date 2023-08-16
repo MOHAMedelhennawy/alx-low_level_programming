4675b6aae5f1a51b27ac5f65983317d1e789c2e1
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-16 10:56:22 -0700

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - func to print name
+ * @name: data to store the name
+ * @f: a function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
