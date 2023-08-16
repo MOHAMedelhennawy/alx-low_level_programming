ab877ce338ca97c4e74edcc503315ba4b37871a0
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-16 12:30:20 -0700

    initial commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
+	if (size <= 0)
+		return (-1);
+
+	if (array != NULL && cmp != NULL)
+	{	
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
 	}
-
 
 	return (-1);
 }
