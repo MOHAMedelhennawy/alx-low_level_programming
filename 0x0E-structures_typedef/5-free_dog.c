a235697a9c8e0b0a1cbcb86030109a38fcb9eea6
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-18 13:46:05 -0700

    initial commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * free_dog - frees the memory of dog struct.
+ * @d: dog struct
+ * Return: pointer to the new dog.
+ */
 void free_dog(dog_t *d)
 {
 	if (!d)
