50de5e5363956a9455c913764c101562dd341409
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-24 23:30:14 -0700

    Upload 100-first.c file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * before_main - type of function that are executed before main function
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
