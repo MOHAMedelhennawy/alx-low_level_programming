c2042675ff5f34df7659a9fc4fbb8628d5f9079f
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 06:06:52 -0700

    [Fix]

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,11 +2,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *pop;
+	int i;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 		return (0);
 	pop = *head;
+	i = (*head)->n;
 	*head = (*head)->next;
 	free(pop);
-	return ((*head)->n);
+	return (i);
 }
