1f3c8fee9003c102a0160529574910dfbd5dec7a
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 07:09:19 -0700

    [Add]: task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - get sum
+ * @head: pointer to fist node
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *ptr;
+
+	if (!head)
+		return (0);
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
