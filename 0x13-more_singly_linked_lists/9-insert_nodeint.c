f02a2e9abedc04595dd163f97a2f9d4057f708c1
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 07:57:11 -0700

    [Fix]: betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *prev_node, *next_node, *new_node;
 	unsigned int count = 0;
-	
+
 	prev_node = *head;
 	next_node = *head;
 	if (*head == NULL)
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new_node || idx > count)
 		return (NULL);
 
-	prev_node = *head;
+	prev_node = *headi;
 	new_node->n = n;
 	while (idx)
 	{
