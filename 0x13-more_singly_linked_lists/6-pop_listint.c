5c6d23d9983a49bae2f1e0e28143f87fd0f759e4
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 06:02:25 -0700

    [Fix]

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,12 @@
 #include "lists.h"
 int pop_listint(listint_t **head)
 {
-	int i = 0;
-	listint_t *h, *prev_node, *next_node;
+	listint_t *pop;
 
-	h = *head;
-	prev_node = *head;
-	next_node = *head;
-	while (next_node->next != NULL)
-	{
-		prev_node = next_node;
-		next_node = next_node->next;
-		i++;
-	}
-
-	free(next_node);
-	next_node = NULL;
-	prev_node->next = NULL;
-	return (h->n);
+	if (*head == NULL || head == NULL)
+		return (0);
+	pop = *head;
+	*head = (*head)->next;
+	free(pop);
+	return ((*head)->n);
 }
