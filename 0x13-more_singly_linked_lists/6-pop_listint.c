61839ed4a9c7db6f26946a6a751d79dc4302d5df
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 06:13:22 -0700

    Fix betty style

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * pop_listint - Delete fist node
+ * @head: pointer to head
+ * Return: the head node’s data (n)
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *pop;
