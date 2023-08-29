e7a5d016defa7bf8e26213f064a40399d21aa5f4
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 07:14:36 -0700

    [Fix]

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,11 +2,12 @@
 /**
  * sum_listint - get sum
  * @head: pointer to fist node
+ * Return: sum of member n
  */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *ptr;
+	listint_t *ptr = head;
 
 	if (!head)
 		return (0);
