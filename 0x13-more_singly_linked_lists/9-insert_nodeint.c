07168c94d1e7a6fa26802fcd1c2eefe39578d5af
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-29 08:21:32 -0700

    [Fix]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,9 +21,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev_node = prev_node->next;
 		count++;
 	}
-
+	
+	if (idx > count)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node || idx > count)
+	if (!new_node)
 		return (NULL);
 
 	prev_node = *head;
