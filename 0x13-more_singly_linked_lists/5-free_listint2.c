003c0ee9db7e3ad484f1843bd10eb8bf1514a0e9
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-28 23:32:39 -0700

    [Fix]

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,13 @@
 #include "lists.h"
-
+/**
+ * free_listint2 - destroy nodes.
+ * @head: pointer to head pointer
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *next, *current;
 
-	current = *head;
+	next = *head;
 	while (current != NULL)
 	{
 		current = next;
