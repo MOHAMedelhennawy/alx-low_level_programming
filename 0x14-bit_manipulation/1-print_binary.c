f12515e8ee17fc20e8d95416d2423c82a69b2278
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-31 10:12:26 -0700

    [Add] task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_binary - translate number to binary.
+ * @n: the input number
+ * Return: binary number started by one
+ */
+void print_binary(unsigned long int n)
+{
+	int i, j = 0;
+	unsigned long int temp, mask = 0;
+
+	if (n == 0)
+		_putchar('0');
+	temp = n;
+
+	for (j = 0; temp != 0; j++)/*Ignoring left-zeros*/
+		temp = temp >> 1;
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		mask = 1 << i;/*Creating a mask for extract the digit*/
+		if ((n & mask) != 0)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
+
