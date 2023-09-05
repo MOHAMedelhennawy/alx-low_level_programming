6a9feb21e7d7536e9e1702238f369d034f08a15f
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-09-05 03:57:59 -0700

    [Add] 0-task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * read_textfile - that reads a text file and prints
  * @filename: variable pointer
@@ -10,29 +9,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+	ssize_t  file_descriptor, bytes;
+	char *ch;
 
-	text = malloc(letters);
-	if (text == NULL)
+	if (!filename)
 		return (0);
 
-	if (filename == NULL)
+	ch = malloc(sizeof(char) * letters);
+	if (!ch)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-	{
-		free(text);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
-	}
-
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
-
-	close(file);
-
-	return (w);
+	bytes= read(file_descriptor, ch, letters);
+        write(STDOUT_FILENO, ch, bytes);
+	
+	close(file_descriptor);
+	free(ch);
+	return (bytes);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
