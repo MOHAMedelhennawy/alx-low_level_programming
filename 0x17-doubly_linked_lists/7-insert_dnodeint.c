e970774ded9ad79411bcc91f77364c355b1a54a1
Mohamed elhennawy <el7ennawy2002@gmail.com>
2023-10-12 21:51:20 +0300

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	ptr = *h;
 	if (idx == size)
+	{
 		add_dnodeint_end(h, n);
+		return (*h);
+	}
 	else
 	{
 		/*create a new node to insert*/
