f8b7e4758b3c900605de7c1bf0181856f47650dd
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-07 20:39:22 +0200

    Fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * get_dnodeint_at_index - to Return node at index.
+ * @head: head of linked list.
+ * Return: indexed node
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *ptr = head;
@@ -6,8 +11,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (index--)
 	{
 		ptr = ptr->next;
-		if (ptr == NULL)
-			return NULL;
 	}
 	return (ptr);
 }
