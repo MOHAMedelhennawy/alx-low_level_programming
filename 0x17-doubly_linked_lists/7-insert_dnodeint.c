38fac64bbe3aadec9b1880914b55df77f1a95f59
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-07 23:25:59 +0200

    Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,11 +30,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx > siz)
 		return (NULL);
 
-	while ((idx--) - 1)
+	while ((idx--) - 1 || slow)
 	{
 		slow = slow->next;
 		fast = slow->next;
-		if (fast == NULL || slow == NULL)
+		if (!slow)
 			return (NULL);
 	}
 
