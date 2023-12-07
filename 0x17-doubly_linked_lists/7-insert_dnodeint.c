5452ee9fb55551ab534575fe87c2b71b4fc07a95
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-07 23:42:07 +0200

    Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,58 +1,49 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - to insert the node
- * @h: header node
- * @idx: the index
- * @n: n value of new node
- * Return: Always EXIT_SUCCESS.
+ * insert_dnodeint_at_index - to insert at index idx.
+ * @idx: the required note to insert in.
+ * @h: head of linked list.
+ * @n: value of n.
+ * Return: New item.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *ptr = *h, *ptr2;
-	unsigned int size = 0;
-	/*get the length of list*/
+	dlistint_t *ptr = *h, *new_node, *ptr2;
+	unsigned int siz = 0;
+
+	/*to get the size of list*/
 	while (ptr)
 	{
 		ptr = ptr->next;
-		size++;
+		siz++;
 	}
-	if (h == NULL || idx > size)
-		return (NULL);
-	
+	/*insert at beginning*/
 	if (idx == 0)
-	{
-		new_node = add_dnodeint(h, n);
-		return (new_node);
-	}
-	/*insert node at End*/
-	else if (idx == size)
-	{
-		new_node = add_dnodeint_end(h, n);
-		return (new_node);
-	}
+		return (add_dnodeint(h, n));
+	/*insert at End*/
+	else if (idx == siz)
+		return (add_dnodeint_end(h, n));
+	/*create a new node to insert*/
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-	else
+	if (*h == NULL)
 	{
-		/*create a new node to insert*/
-		new_node = malloc(sizeof(dlistint_t));
-		if (!new_node)
-			return (NULL);
-		if (*h == NULL)
-		{
-			new_node->next = NULL;
-			new_node->prev = NULL;
-			return (new_node);
-		}
-		/*insert a node in idx postion*/
-		new_node->n = n;
-		ptr = *h;
-		while (idx-- > 1 && ptr)
-			ptr = ptr->next;
-		ptr2 = ptr->next;
-		ptr->next = new_node;
-		new_node->prev = ptr;
-		new_node->next = ptr2;
-		ptr2->prev = new_node;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		return (new_node);
 	}
+	/*insert a node in idx postion*/
+	new_node->n = n;
+	ptr = *h;
+	while (idx-- > 1 && ptr)
+		ptr = ptr->next;
+	ptr2 = ptr->next;
+	ptr->next = new_node;
+	new_node->prev = ptr;
+	new_node->next = ptr2;
+	ptr2->prev = new_node;
+
 	return (new_node);
 }
