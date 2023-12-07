7bf0b05757cca31ba9433d2f1a0d7d517ab3f76a
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-07 23:13:42 +0200

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,20 +30,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx > siz)
 		return (NULL);
 
-
-	
-	while ((idx--) - 1)
+	while (idx--)
 	{
 		slow = slow->next;
 		fast = slow->next;
 	}
-	
+
 	new_node->n = n;
 	new_node->next = fast;
 	new_node->prev = slow;
 	fast->prev = new_node;
 	slow->next = new_node;
-	
 
 	return (new_node);
 }
