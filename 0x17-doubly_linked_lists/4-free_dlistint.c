38b2a06ea2cddc512057b7e1a570120ad57d883b
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-07 20:21:49 +0200

    FixBettyStyle

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,18 @@
 #include "lists.h"
+/**
+ * free_dlistint - to Free the All list.
+ * @head: head of linked list.
+ */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *next;
+	dlistint_t *next;
 
-    while (next)
-    {
-        next = head->next;
-        free(head);
-        head = next;
-    }
+	if (head == NULL)
+		return;
+	while (next)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
 }
