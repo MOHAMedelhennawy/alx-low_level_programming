9437ca717e8436d481606fd803d93c4401eb97c9
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-11 10:48:04 +0200

    Fix

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,19 +1,65 @@
-int add(int a, int b) {
-    return a + b;
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+/**
+* add - adds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the sum
+*/
+
+int add(int i, int k)
+{
+	return (i + k);
 }
 
-int sub(int a, int b) {
-    return a - b;
+/**
+* sub - subtracts two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the subtract
+*/
+
+int sub(int i, int k)
+{
+	return (i - k);
 }
 
-int mul(int a, int b) {
-    return a * b;
+/**
+* mul - multiplies two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the multiple
+*/
+
+int mul(int i, int k)
+{
+	return (i * k);
 }
 
-int div(int a, int b) {
-    return a / b;
+/**
+* div - divideds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the dividend integer
+*/
+
+int div(int i, int k)
+{
+	return (i / k);
 }
 
-int mod(int a, int b) {
-    return a % b;
+/**
+  mod - finds the modulus of two integers
+  @i: first integer to add
+  @k: second integer to add
+  Return: the modulus
+*/
+
+int mod(int i, int k)
+{
+	return (i % k);
 }
