59a7ae31f3a85e132707a2466be7e243d546dc05
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-12 03:59:14 +0200

    Add

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,5 @@
-#include <stdlib.h>
 #include "function_pointers.h"
-/**
- * print_name - func to print name
- * @name: data to store the name
- * @f: a function pointer
- */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+    f(name);
 }
