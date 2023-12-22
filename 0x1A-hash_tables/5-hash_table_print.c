676f668ad29878bb713cc84cb54e5c14668e7c8b
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 18:45:22 +0200

    Add

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,27 +9,26 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *trv;
 	unsigned long int index = 0, flag = 0;
 
-	if (ht)
-	{
-		printf("{");
-		while (index != ht->size)
-		{
-			if (ht->array[index] != NULL)
-			{
-				trv = ht->array[index];
-				while (trv != NULL)
-				{
-					if (flag == 1)
-						printf(", ");
-					printf("'%s': '%s'", trv->key, trv->value);
-					flag = 1;
-					trv = trv->next;
-				}
-			}
-			index++;
-		}
-		printf("}\n");
-	}
+	if (!ht)
+		return;
 
-	return;
+	printf("{");
+	while (index != ht->size)
+	{
+		if (ht->array[index] != NULL)
+		{
+			trv = ht->array[index];
+			while (trv != NULL)
+			{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s': '%s'", trv->key, trv->value);
+				flag = 1;
+				trv = trv->next;
+			}
+		}
+		index++;
+	}
+	printf("}\n");
+
 }
