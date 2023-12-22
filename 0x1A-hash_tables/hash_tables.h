f385894ae25e6a5e0832ed14064cba0a53e82ee0
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 18:46:58 +0200

    Add

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,3 +37,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *creat_new_item(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
