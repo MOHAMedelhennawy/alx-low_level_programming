b614875c87fa14170181ba62b5b3944c210386f8
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 18:49:44 +0200

    Fix

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,5 @@
+#ifndef __HASH_TABLES__
+#define __HASH_TABLES__
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,9 +13,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,8 +28,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -38,3 +40,4 @@ hash_node_t *creat_new_item(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+#endif
