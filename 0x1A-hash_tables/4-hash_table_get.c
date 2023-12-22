4443961c1d7179a111e823e721a077f643a02008
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 17:22:44 +0200

    Add

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *trv;
 
-	if (ht == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || strcmp(key, "") == 0 || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
