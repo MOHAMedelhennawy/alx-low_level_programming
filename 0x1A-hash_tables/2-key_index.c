b61cf3921aabe7de9de679a549750d50b83ee2b3
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 15:06:30 +0200

    FixBettyStyle

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,8 +1,14 @@
 #include "hash_tables.h"
+/**
+ * key_index - to return the index of the node
+ * @key: the key that will hashing
+ * @size: size of array
+ * Return: return the index of key
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
-    index = hash_djb2(key);
+	unsigned long int index;
 
-    return index % size;
+	index = hash_djb2(key);
+	return (index % size);
 }
