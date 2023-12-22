9f239c51a83ebe7ec890f9c0b4c04022b78d1150
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 18:42:02 +0200

    Add

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+void hash_table_print(const hash_table_t *ht)
+{
+    hash_node_t *trv;
+    unsigned long int index = 0, flag = 0;
+
+    printf("{");
+    while (index != ht->size)
+    {
+        if (ht->array[index] != NULL)
+        {
+            trv = ht->array[index];
+            while (trv != NULL)
+            {
+                if (flag == 1)
+                    printf(", ");
+                printf("'%s': '%s'", trv->key, trv->value);
+                flag = 1;
+                trv = trv->next;
+            }
+        }
+        index++;
+    }
+    printf("}\n");
+
+}
