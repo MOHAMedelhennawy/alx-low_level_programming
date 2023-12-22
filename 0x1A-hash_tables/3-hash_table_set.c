c2cddf8e1f143bd7a36ba0fcc9c15016dea59db7
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-22 16:21:14 +0200

    Add

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,9 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
             free(new_item);
             return (0);
         }
-        free(new_item->key);
-        free(new_item->value);
-        free(new_item);
+
         return (1);
     }
 
