c4073707b383493e089fb042b5aeb4e103174ccb
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-12-20 11:32:22 +0200

    Add First Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,20 @@
 #include "lists.h"
-/**
- * print_list - to access the data of a node and print them
- * @h: pointer to the list address
- * Return: the number of nodes
- */
-size_t print_list(const list_t *h)
-{
+
+size_t print_list(const list_t *h) {
+
 	size_t count = 0;
+	const list_t *ptr = h;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
-	{
-		count++;
-		if (h->str == NULL)
+	while (ptr) {
+		if (ptr->str) {
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		} else {
 			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+		}
+
+		ptr = ptr->next;
+		count++;
 	}
-	return (count);
+
+	return count;
 }
