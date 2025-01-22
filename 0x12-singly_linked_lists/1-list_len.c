92906f04a62e5ec6ce7336145b8af1c886981f13
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2025-01-22 04:40:09 +0200

    Add: Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,11 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
