9eddf76b6f6dcc9e12b07689d183efdeca4cfd7d
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2025-01-23 11:28:18 +0200

    Add: Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,41 +8,31 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *aux_node = *head;
-	listint_t *new_node;
-	unsigned int index;
-	unsigned int cont = 0;
+	listint_t *tmp, *trv;
 
-	/* create node */
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
+	if (!head || !*head)
+		return 	(NULL);
+	
+	trv = *head;
+	tmp = (listint_t *)malloc(sizeof(listint_t));
+	tmp->n = n;
 
-	/* border case for insert at the beginning */
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (*head);
+		tmp->next = NULL;
+		*head = tmp;
+		return (tmp);
 	}
 
-	/* search of position to insert */
-	index = idx - 1;
-	while (aux_node && cont != index)
+	while (--idx)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		if (!trv->next)
+			return (NULL);
+		trv = trv->next;
 	}
 
-	/* general case */
-	if (cont == index && aux_node)
-	{
-		new_node->next = aux_node->next;
-		aux_node->next = new_node;
-		return (new_node);
-	}
+	tmp->next = trv->next;
+	trv->next = tmp;
 
-	free(new_node);
-	return (NULL);
+	return (tmp);
 }
