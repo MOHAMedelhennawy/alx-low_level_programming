92897ce3a4d9af00dd87abb84777f44918a3926c
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2025-01-23 09:56:01 +0200

    Add: Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,8 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	trv = head;
-	while (index-- && trv->next)
+	while (index--)
+	{
+		if (!trv->next)
+			return (NULL);
 		trv = trv->next;
+	}
 
 	return (trv);
 }
