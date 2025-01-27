efc5adaa240964e076fbcda67bd04f180d33962d
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2025-01-27 08:26:02 +0200

    Add: Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,75 +8,46 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *head;
+	hash_node_t *new_item;
+	hash_node_t **hash_table_array;
 	unsigned long int index;
-	hash_node_t *new_item, *current_item;
 
-	new_item = creat_new_item(key, value);
-	if (strcmp(key, "") == 0 || !ht || !new_item)
+	if (!ht || !key)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	/*If array[index] is empty*/
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_item;
-		return (1);
-	}
+	hash_table_array = ht->array;
+	index = key_index((unsigned char *)key, ht->size);
+	head = hash_table_array[index];
+	new_item = creat_new_item(&head, key, value);
 
-	/*If current_item is actuly exist raplace his value*/
-	current_item = ht->array[index];
-	if (strcmp(current_item->key, key) == 0)
-	{
-		current_item->value = strdup(value);
-		if (current_item->value == NULL)
-		{
-			free(new_item->key);
-			free(new_item->value);
-			free(new_item);
-			return (0);
-		}
+	if (!new_item)
+		return (0);
 
-		return (1);
-	}
-
-	/*insert at beginning*/
-	new_item->next = current_item;
-	ht->array[index] = new_item;
 	return (1);
-
 }
 
-/**
- * creat_new_item - to creates a new item
- * @key: key of new item.
- * @value: value of new item.
- * Return: new item in succeeded, NULL otherwise.
- */
-hash_node_t *creat_new_item(const char *key, const char *value)
+hash_node_t *creat_new_item(
+	hash_node_t **head, const char *key, const char *value)
 {
-	hash_node_t *new_item;
+	hash_node_t *new_item = (hash_node_t *)malloc(sizeof(hash_node_t));
+	hash_node_t *ptr = *head;
 
-	new_item = (hash_node_t *) malloc(sizeof(hash_node_t));
-	if (new_item == NULL)
+	if (!new_item)
 		return (NULL);
 
-	new_item->key = (char *) malloc(sizeof(key));
-	new_item->key = strdup(key);
-	if (new_item->key == NULL)
-	{
-		free(new_item);
-		return (NULL);
-	}
-
-	new_item->value = (char *) malloc(sizeof(value));
-	new_item->value = strdup(value);
-	if (new_item->value == NULL)
-	{
-		free(new_item);
-		free(new_item->key);
-		return (NULL);
-	}
-
+	new_item->key = (char *) key;
+	new_item->value = (char *) value;
 	new_item->next = NULL;
+
+	if (*head == NULL)
+		*head = new_item;
+	else
+	{
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = new_item;
+	}
+
 	return (new_item);
 }
